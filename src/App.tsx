6c344f48b0291b9d0db636c9fa7b61b80e5fbf84
HartJN <jameshart04@gmail.com>
2023-02-28 11:30:15 +1300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+
+function generateNestedScss(html: string): string {
+  const div = document.createElement('div');
+  div.innerHTML = html;
+
+  const element = div.querySelector('[class], [className]');
+  const tagName = element
+    ? ''
+    : div.querySelector('*')?.tagName.toLowerCase() || '';
+
+  const firstMatch =
+    (
+      element?.getAttribute('class') ||
+      element?.getAttribute('className') ||
+      ''
+    ).match(/^\S+/)?.[0] || '';
+
+  let scss = `${firstMatch ? `.${firstMatch}` : tagName} {\n`;
+
+  const classNames = new Set<string>();
+  for (const child of element?.children || []) {
+    const childScss = generateNestedScss(child.outerHTML);
+    if (!classNames.has(childScss)) {
+      classNames.add(childScss);
+      scss += childScss;
+    }
+  }
+
+  return `${scss}}\n`;
+}
+
+function App() {
+  const [inputHtml, setInputHtml] = useState('');
+  const [generatedScss, setGeneratedScss] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInputHtml(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const scss = generateNestedScss(inputHtml);
+    setGeneratedScss(scss);
+  };
+
+  return (
+    <div className="app-container">
+      <h1 className="app-title">HTML to SCSS Converter</h1>
+      <div className="app-content">
+        <div className="input-container">
+          <label htmlFor="html-input">Enter your HTML:</label>
+          <textarea
+            id="html-input"
+            value={inputHtml}
+            onChange={handleInputChange}
+            rows={30}
+            cols={50}
+          />
+          <button onClick={handleSubmit}>Generate SCSS</button>
+        </div>
+        {generatedScss && (
+          <div className="generated-scss-container">
+            <label htmlFor="scss-output">Generated SCSS:</label>
+            <textarea
+              onChange={handleInputChange}
+              id="scss-output"
+              rows={30}
+              cols={50}
+              value={generatedScss}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
